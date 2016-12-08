40d08b5611e24fc36a9f94739ade2e26064e5851
root <root@localhost.localdomain>
2016-12-08 20:26:42 +0000

    lisätty templaten käsittely

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
     return "Hello World!!!!!!!!!!!!!!"
 
-@application.route("/hello/<name>")
-def hello_name(name):
-    return "Hello %s!" % name
+@application.route("/hello/<user>")
+def hello_name(user):
+   return render_template('hello.html', name = user)
 
 if __name__ == "__main__":
     application.run()
