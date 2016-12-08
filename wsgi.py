2e65302a3c91ceda7438683d9d426e0708e2cf94
root <root@localhost.localdomain>
2016-12-08 20:49:25 +0000

    vielä lisätty muokatu ohjelma

--- wsgi.py
+++ wsgi.py
@@ -9,5 +9,10 @@ def hello():
 def hello_name(user):
    return render_template('hello.html', name = user)
 
+@application.route("/result")
+def result():
+   dict = {'phy':50,'che':60,'maths':70}
+   return render_template('result.html', result = dict)
+
 if __name__ == "__main__":
     application.run()
